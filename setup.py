f757a27d87cea28be940c037504b20fa125cd252
Pallob Poddar <51057880+PallobPoddar@users.noreply.github.com>
2022-12-25 23:41:04 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+import model
+
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name="model",
+        version=model.__version__,
+        description="Model: Collection of models",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author="Pallob Poddar",
+        author_email="pallob.poddar@northsouth.edu",
+        url="https://github.com/PallobPoddar/Ocular-Disease-Detection",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        platforms=["linux", "unix"],
+        python_requires=">3.5.2",
+        install_requires=["scikit-learn>=0.22.1"],
+    )
